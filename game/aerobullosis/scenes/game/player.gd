19c875d71e3bb16913bcd00eacf7ef99b82870b2
CKolean <datahaavi@outlook.com>
2025-01-25 10:13:01 +0200

    adding rudimentary character controller

---
+++ game/aerobullosis/scenes/game/player.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+var start_position
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	start_position = screen_size
+	print(start_position)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		print("right")
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		print("left")
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		print("up")
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
