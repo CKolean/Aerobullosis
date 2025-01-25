95399937c449944291a67b33aaa9801017e68379
arhi <arhi.makkonen@gmail.com>
2025-01-26 00:28:32 +0200

    added player animations

--- game/aerobullosis/scenes/game/player.gd
+++ game/aerobullosis/scenes/game/player.gd
@@ -5,6 +5,7 @@ var start_position
 @export var start_rotation = 92
 @export var absolute_player_depth_px: float = 0.0
 @export var can_sink: bool = true
+@onready var animation_tree : AnimationTree = get_node("PlayerSprite2D/AnimationPlayer/AnimationTree")
 
 var rotation_limit = 15
 var right_rotation = 0.005
@@ -23,9 +24,9 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	#start_position = Vector2(screen_size[0]/2,(screen_size[1]-100))
 	#position = start_position
-	global_rotation_degrees = start_rotation
+	#global_rotation_degrees = start_rotation
 	#turning bubbles on
-	$ParticleBubbles.emitting = true
+	#$ParticleBubbles.emitting = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -34,21 +35,29 @@ func _process(delta):
 		#gravity
 	if not Input.is_anything_pressed() and can_sink==true:
 		velocity.y += sea_gravity
+		animation_tree["parameters/conditions/sinking"] = true
+		animation_tree["parameters/conditions/moving"] = false
+	else:
+		animation_tree["parameters/conditions/sinking"] = false
+		animation_tree["parameters/conditions/moving"] = true
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x += right_strenght
 		velocity.y -= right_rotation
 		velocity == velocity.rotated(right_strenght)
+		animation_tree["parameters/conditions/start"] = true
 		if not global_rotation_degrees > rotation_limit:
 			rotation = rotation + right_rotation
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= left_strenght
 		velocity.y -= left_strenght
 		velocity += velocity.rotated(left_strenght)
+		animation_tree["parameters/conditions/start"] = true
 		if not global_rotation_degrees <-rotation_limit:
 			rotation = rotation - left_rotation
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= up_strenght
+		animation_tree["parameters/conditions/start"] = true
 		if global_rotation_degrees >0:
 			rotation = rotation_correction_strenght
 		else:
